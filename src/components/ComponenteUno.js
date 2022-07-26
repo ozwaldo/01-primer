8e373c793b1af7646bbe951398f7f211067df70b
oswaldo <ozwaldo@gmail.com>
2022-07-26 16:17:36 -0500

    uno

---
+++ src/components/ComponenteUno.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ComponenteUno = () => {
+    return (
+        <div className='componenteuno'>
+            <h1>Componente Uno</h1>
+        </div>
+    )
+}

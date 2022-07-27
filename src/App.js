8c04ea28a42f246fcbf040f4b41e1607b6dcab53
oswaldo <ozwaldo@gmail.com>
2022-07-27 13:58:15 -0500

    Nuevo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { ComponenteUno } from './components/ComponenteUno';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-            Learn React
+            <ComponenteUno></ComponenteUno>
         </a>
       </header>
     </div>
